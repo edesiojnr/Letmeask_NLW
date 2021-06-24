27d0bbf39a6414216ad6b2bd7b987566cbb248bf
edesiojnr <edesiojnr@gmail.com>
2021-06-24 08:55:13 -0300

    Second day NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthcontextProvider } from './contexts/AuthContext'
 
 
 function App() {
-  return (
-    <Home />
+  
+  
+   return (
+    <BrowserRouter>
+      <AuthcontextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthcontextProvider>
+    </BrowserRouter>
   );
 }
 
